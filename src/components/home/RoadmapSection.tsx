78f5ed07ee75f763cc621a3a3af0876cc89287ae
Md-Yousuf-sheikh <sheikhyousuf702@gmail.com>
2025-05-14 00:56:47 +0600

    resolve: roadmap

--- src/components/home/RoadmapSection.tsx
+++ src/components/home/RoadmapSection.tsx
@@ -1,131 +1,116 @@
 import { ChevronRight } from "lucide-react";
 
+const roadmapPhases = [
+  {
+    phase: "Phase 1",
+    title: "Launch",
+    items: [
+      "Token creation on Solana",
+      "Website and whitepaper release",
+      "Distribution via Distribute Listings on Raydium and DEXTools",
+    ],
+  },
+  {
+    phase: "Phase 2",
+    title: "Community Expansion",
+    items: [
+      "Mortal Game (PvP prototype) launch",
+      "First tournament",
+      "Begin 5% SOL rewards via Distribute for holders",
+    ],
+  },
+  {
+    phase: "Phase 3",
+    title: "Mortal Coin Staking",
+    items: [
+      "Mortal Game (PvP prototype) launch",
+      "First tournament",
+      "Begin 5% SOL rewards via Distribute for holders",
+    ],
+  },
+  {
+    phase: "Phase 4",
+    title: "Beta Game Launch",
+    items: [
+      "Mortal Game (PvP prototype) launch",
+      "First tournament",
+      "Begin 5% SOL rewards via Distribute for holders",
+    ],
+  },
+];
+
 export default function RoadmapSection() {
   return (
     <div className="bg-[url('/img/Back_6.jpg')] bg-cover bg-center bg-no-repeat bg-blend-overlay">
-      <div className="section-padding bg-shadow-blinder-y  px-4 sm:px-8">
+      <div className="section-padding bg-shadow-blinder-y px-4 sm:px-8">
         <div className="max-w-6xl mx-auto text-center text-white">
-          {/* Title */}
-          <h1 className="text-4xl sm:text-5xl font-extrabold mb-4">Roadmap</h1>
-          {/* content */}
-          <div className=" md:flex pt-14">
-            {/* Overlay for text readability */}
-            <div data-aos="fade-up-right"  className="flex justify-center w-full lg:w-[45%] xl:w-[50%]">
+          <h1 className="section-title">Roadmap</h1>
+          <div className="md:flex pt-14">
+            <div
+              data-aos="fade-up-right"
+              className="flex justify-center w-full lg:w-[45%] xl:w-[50%]"
+            >
               <img
                 src="/img/coin-logo.png"
-                className=" h-[300px] lg:h-[80%] object-contain "
+                className="h-[300px] lg:h-[80%] object-contain"
+                alt="Coin Logo"
               />
             </div>
-            {/* time line */}
-            <div className="">
+
+            <div className="w-full lg:w-[55%] xl:w-[50%]">
               <ol data-aos="fade-up">
-                {/* section line */}
-                <li  className="border-l-2 border-white  mt-3 ">
-                  <div className="md:flex flex-start">
-                    {/* line and icon */}
-                    <div className="bg-white w-6 h-6 flex items-center justify-center rounded-full -ml-3.5 ">
-                      <ChevronRight color="black" size={15} />
-                    </div>
-                    {/* box info */}
-                    <div className="block  p-3 lg:p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-5  lg:ml-6  md:mb-10 -mt-10 text-left">
-                      <div className="justify-between  w-full ">
-                        <p className="font-normal text-gray-400  text-sm  md:text-xl lg:text-2xl uppercase">
-                          Phase 1
-                        </p>
-                        <h1 className="timeline-title">
-                          Launch
-                        </h1>
+                {roadmapPhases.map((phaseData, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className={`border-l-1 border-[#2975f1ff] ${
+                        index === 0 ? "mt-10" : "pt-0"
+                      } `}
+                    >
+                      <div className="md:flex flex-start ">
+                        <div
+                          className={`${
+                            index === 0 ? "mt-0" : "mt-12"
+                          } bg-[#2975f1ff] w-6 h-6 flex items-center justify-center rounded-full -ml-3.5`}
+                        >
+                          <ChevronRight color="black" size={15} />
+                        </div>
+
+                        {/* info content */}
+                        <div
+                          className={`${
+                            index === 0 ? "-mt-10" : "pt-10"
+                          } block p-3 lg:p-6 rounded-lg shadow-lg bg-[#222428ff] max-w-md ml-5 lg:ml-6 md:mb-10 text-left`}
+                        >
+                          <div className="w-full">
+                            <p className="font-medium text-white text-sm md:text-md lg:text-lg uppercase">
+                              {phaseData.phase}
+                            </p>
+                            <h1 className="timeline-title">
+                              {phaseData.title}
+                            </h1>
+                          </div>
+
+                          <ul className="-space-y-3">
+                            {phaseData.items.map((item, idx) => (
+                              <li
+                                key={idx}
+                                className="flex space-x-1 items-center"
+                              >
+                                <p className="text-[40px] -mt-1 text-[#2975F1]">
+                                  &#x2022;
+                                </p>
+                                <p className="text-sm md:text-base xl:text-[20px]">
+                                  {item}
+                                </p>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
                       </div>
-
-                      {/* info */}
-                      <p className="text-gray-700 mb-2   text-base   lg:text-lg  xl:text-xl ">
-                        Enter the ultimate battleground where strength is the
-                        only currency. Connect your wallet, join the fight, and
-                        claim your place among the champions.
-                      </p>
-                    </div>
-                    {/* end box */}
-                  </div>
-                </li>
-
-                {/* section line 2 */}
-                <li className="border-l-2 border-white  ">
-                  <div className="md:flex flex-start">
-                    {/* line and icon */}
-                    <div className="bg-white w-6 h-6 flex items-center justify-center rounded-full -ml-3.5 ">
-                      <ChevronRight color="black" size={15} />
-                    </div>
-                    {/* box info */}
-                    <div className="block  p-3 lg:p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-5  lg:ml-6  md:mb-10  text-left">
-                      <div className="justify-between  w-full ">
-                        <p className="font-normal text-gray-400  text-sm  md:text-xl lg:text-2xl uppercase">
-                          Phase 2
-                        </p>
-                        <h1 className="timeline-title">Community Expansion</h1>
-                      </div>
-
-                      {/* info */}
-                      <p className="text-gray-700 mb-2   text-base   lg:text-lg  xl:text-xl ">
-                        Enter the ultimate battleground where strength is the
-                        only currency. Connect your wallet, join the fight, and
-                        claim your place among the champions.
-                      </p>
-                    </div>
-                    {/* end box */}
-                  </div>
-                </li>
-                {/* section line 3 */}
-                <li className="border-l-2 border-white  ">
-                  <div className="md:flex flex-start">
-                    {/* line and icon */}
-                    <div className="bg-white w-6 h-6 flex items-center justify-center rounded-full -ml-3.5 ">
-                      <ChevronRight color="black" size={15} />
-                    </div>
-                    {/* box info */}
-                    <div className="block  p-3 lg:p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-5  lg:ml-6  md:mb-10  text-left">
-                      <div className="justify-between  w-full ">
-                        <p className="font-normal text-gray-400   text-base  md:text-xl lg:text-2xl uppercase">
-                          Phase 3
-                        </p>
-                        <h1 className="timeline-title">Mortal Coin Staking</h1>
-                      </div>
-
-                      {/* info */}
-                      <p className="text-gray-700 mb-2   text-base   lg:text-lg  xl:text-xl ">
-                        Enter the ultimate battleground where strength is the
-                        only currency. Connect your wallet, join the fight, and
-                        claim your place among the champions.
-                      </p>
-                    </div>
-                    {/* end box */}
-                  </div>
-                </li>
-                {/* section line 4 */}
-                <li className="border-l-2 border-white  ">
-                  <div className="md:flex flex-start">
-                    {/* line and icon */}
-                    <div className="bg-white w-6 h-6 flex items-center justify-center rounded-full -ml-3.5 ">
-                      <ChevronRight color="black" size={15} />
-                    </div>
-                    {/* box info */}
-                    <div className="block  p-3 lg:p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-5  lg:ml-6    text-left">
-                      <div className="justify-between  w-full ">
-                        <p className="font-normal text-gray-400  text-base  md:text-xl lg:text-2xl uppercase">
-                          Phase 1
-                        </p>
-                        <h1 className="timeline-title">Beta Game Launch</h1>
-                      </div>
-
-                      {/* info */}
-                      <p className="text-gray-700 mb-2   text-base   lg:text-lg  xl:text-xl ">
-                        Enter the ultimate battleground where strength is the
-                        only currency. Connect your wallet, join the fight, and
-                        claim your place among the champions.
-                      </p>
-                    </div>
-                    {/* end box */}
-                  </div>
-                </li>
+                    </li>
+                  );
+                })}
               </ol>
             </div>
           </div>
